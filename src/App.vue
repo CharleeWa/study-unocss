00a25b7958f694699c662fcdb7d2f31773bb7b9e
Charlie Wang <18888351756@163.com>
2022-10-10 13:18:00 +0800

    :tada: `unocss` 以及周边工具链 初体验

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="i-ic-baseline-add-circle text-3xl bg-green-500" />
+  <button
+    p="y-2 x-4"
+    font="semibold"
+    shadow="lg"
+    text="white"
+    bg="green-500 hover:green-700"
+    border="rounded-lg none "
+    cursor="pointer"
+  >
+    Click me
+  </button>
+</template>
